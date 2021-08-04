240c8755fde015f81ad1b5ec8e0f90788f043024
lijiale <1157221853@qq.com>
2021-08-05 01:19:07 +0800

    关于注册中心

--- mall-coupon/src/main/java/com/learn/mall/coupon/controller/CouponController.java
+++ mall-coupon/src/main/java/com/learn/mall/coupon/controller/CouponController.java
@@ -25,15 +25,35 @@ import com.learn.common.utils.R;
  * @email 18066550996@163.com
  * @date 2021-04-09 20:34:26
  */
+@SuppressWarnings("all")
 @RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
+
     @Autowired
     private CouponService couponService;
 
     /**
-     * 测试配置中心
+     * TODO：配置中心的使用说明：
+     *
+     * @RefreshScope：配置自动刷新注解 使用Nacos配置中心的步骤：
+     * 首先建立一个bootstrap.properties文件，这个配置文件的在spingboot的加载顺序中早于application.properties
+     * 在bootstrap.properties中配置Nacos的地址和本微服务的服务名称
+     * 然后在springboot项目启动的时候会有日志打出，说它会默认到 [应用名].properties文件中去读取配置
+     * 那么我们就可以在Nacos中建立这个文件，将我们要动态修改的配置搬家到Nacos中，配置中心中改完配置以后一发布
+     * 然后加上 @RefreshScope注解，表示配置刷新，最终就能看到配合修改完毕的效果
+     * 注意：如果配置中心有，会优先使用配置中心的
+     * <p>
+     * 配置中心的细节：
+     * 1）.命名空间：
+     * 做配置隔离,默认有一个public命名空间，默认新增的配置都在public命名空间
+     * 例子：开发环境的配置空间，测试环境的，生产环境的（dev,test,prod）
+     * 在配置中心中选择特定的命名空间，比如有开发环境的配置，测试环境的配置等
+     * spring.cloud.nacos.config.namespace=94faffaa-9692-44b7-9e98-72cc5891c301
+     * 2）.配置集
+     * 3）.配置集ID
+     * 4）.配置分组
      */
     @Value("${coupon.user.name}")
     private String name;
