e32824579cfcebdfc3333a574831ca70f9bcca0e
lijiale <1157221853@qq.com>
2021-08-22 19:10:46 +0800

    rabbitmq注意点

--- mall-order/src/test/java/com/learn/mall/order/MallOrderApplicationTests.java
+++ mall-order/src/test/java/com/learn/mall/order/MallOrderApplicationTests.java
@@ -17,6 +17,7 @@ import java.util.Date;
 @SpringBootTest
 @Slf4j
 @RunWith(SpringRunner.class)
+@SuppressWarnings("all")
 public class MallOrderApplicationTests {
 
     @Autowired
@@ -25,6 +26,12 @@ public class MallOrderApplicationTests {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    /**
+     * TODO:rabbitMQ
+     * 一个队列可以由很多个服务来监听，只要收到消息，队列删除消息，而且只能有一个服务可以收到此消息
+     * 也就是说假如订单服务有多个复制，订单服务以集群的模式部署，那么只会有一个服务来接收到消息
+     * 在接收消息的方法处理中，只有这一个消息处理完毕了，才可以接收并处理下一个消息
+     */
     @Test
     public void testSendMsg() {
         OrderReturnReasonEntity entity = new OrderReturnReasonEntity();
